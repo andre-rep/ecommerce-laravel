2440f11447ef5eda3ea5a2fb6e4ba6d2d80cc9cc
andren25 <guerreiroandre25@gmail.com>
2021-11-17 09:31:42 -0300

    StoragePublic file updated

--- app/FileManager/StoragePublic.php
+++ app/FileManager/StoragePublic.php
@@ -3,8 +3,20 @@
 namespace App\FileManager;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class StoragePublic
 {
-    
+    private $folder;
+
+    public function __construct($folder)
+    {
+        $this->folder = $folder;
+    }
+
+    public function fileUpload($file = '')
+    {
+        $fileName = $file->getClientOriginalName();
+        Storage::disk('public')->put($fileName, $file);
+    }
 }
