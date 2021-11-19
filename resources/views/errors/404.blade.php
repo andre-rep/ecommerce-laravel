8bec11247cd54591c0b42be363bde033d5091901
andren25 <guerreiroandre25@gmail.com>
2021-11-19 10:57:14 -0300

    404 custom error page added

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.publicLayout')
+@section('content')
+    @include('includes.header')
+    <h2>Oops, essa página não existe!</h2>
+    @include('includes.facilities')
+    @include('includes.footer')
+    @include('includes.copyright')
+@endsection
